00c6b9f9b6ecdcdc328ab0820685354afca62add
RodrigoDutraF88 <rodrigodutraf88@gmail.com>
2026-06-07 13:17:49 -0300

    feat: finish eda1 project 3

---
+++ EDA1_resolucao_de_exercicios/trabalho2-EDA1/tipos.h
@@ -0,0 +1,16 @@
+#ifndef TIPOS_H
+#define TIPOS_H
+
+
+typedef struct {
+    char   cidade[20];
+    float *temperatura;
+} TMedidas;
+
+typedef struct {
+    int       qtde_dias;     
+    int       qtde_medidas;  
+    TMedidas *medidas;       
+} TTemperaturas;
+
+#endif 
